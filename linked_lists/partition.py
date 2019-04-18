4e0f1e1f65ee45e3731b942f1758cde3c9d3e236
Gilbert Gerhold <ggerhold@vo-k6.21-school.ru>
2019-04-18 15:53:06 +0300

    linked lists

---
+++ linked_lists/partition.py
@@ -0,0 +1,17 @@
+# Time: O(n)
+# Space: O(1)
+
+def partition(node, k):
+    tail = node
+    head = node
+    while node is not None:
+        nextNode = node.next_node
+        node.next_node = None
+        if node.value >= k:
+            tail.next_node = node
+            tail = node
+        else:
+            node.next_node = head
+            head = node
+        node = nextNode
+    return head
