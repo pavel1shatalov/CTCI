2f77b170ecadf3ee833ac06e810df03daaf58420
pavel1shatalov <pavel.i.shatalov@gmail.com>
2019-05-03 14:51:45 +0300

    stack of plates

---
+++ 03-stacks-queues/03-stack-of-plates.py
@@ -0,0 +1,33 @@
+from Stack import Stack
+
+class SetOfStacks:
+    def __init__(self, max_height):
+        self.max_height = max_height
+        self.stacks = [Stack()]
+        self.i = 0
+
+    def push(self, value):
+        if self.stacks[self.i].size == self.max_height:
+            self.stacks.append(Stack())
+            self.i += 1
+        self.stacks[self.i].push(value)
+
+    def pop(self, value):
+        if self.stacks[index].is_empty():
+            return None
+        value = self.stacks[self.i].pop()
+        if self.stacks[self.i].is_empty() and self.i != 0:
+            self.stacks.pop()
+            self.i -= 1
+        return value
+    
+    def pop_at(self, index):
+        if  index < 0 or index > self.i:
+            return None
+        if self.stacks[index].is_empty():
+            return None
+        value = self.stacks[index].pop()
+        if self.stacks[index].is_empty() and index != 0:
+            self.stacks.pop(index)
+            self.i -= 1
+        return value
